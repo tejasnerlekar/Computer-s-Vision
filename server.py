6de05a84541f86e01e44625df4924e17b1ba1752
Tejas Nerlekar <62929555+tejasnerlekar@users.noreply.github.com>
2026-09-16 00:17:08 +0530

    01

---
+++ server.py
@@ -0,0 +1,28 @@
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+latest_data = {
+    "message": "Waiting for main computer..."
+}
+
+
+@app.route("/")
+def home():
+    return "Computer's Vision relay is running."
+
+
+@app.route("/update", methods=["POST"])
+def update():
+    global latest_data
+    latest_data = request.json
+    return jsonify({"success": True})
+
+
+@app.route("/latest", methods=["GET"])
+def latest():
+    return jsonify(latest_data)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
